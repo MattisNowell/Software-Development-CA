3a5c4cb366ae139aa87ca7d9106367b95083b12a
NowellM <mattis.nowell@gmail.com>
2021-10-26 16:13:48 +0100

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,38 @@
+
+class Player implements Runnable{
+
+    Int[] hand = {};
+    Int totalWeight = 0;
+
+    Player() {
+        this.hand = takeTen();
+    }
+
+    public void run(){
+
+    }
+
+    public Int[] takeTen() {
+        
+    }
+
+    public void discardPebble(Int pebble) {
+
+    }
+
+    public void logAction(String file) {
+
+    }
+
+    public boolean checkHand() {
+
+    }
+
+    public Int[] getHand() {
+
+    }
+
+    public Int getWeight() {
+        
+    }
+ }
