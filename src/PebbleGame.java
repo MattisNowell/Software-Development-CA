30793fcd02afd34215b8c77afdcc1d109f04dadc
George-Murphy <georgeantoni_murphy@hotmail.com>
2021-11-03 16:42:11 +0000

    03/11/2021
    create CSV files with correct number of pebbles inside, new line for each bag

--- src/PebbleGame.java
+++ src/PebbleGame.java
@@ -1,11 +1,18 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+import java.beans.Transient;
 import java.util.Scanner;
+import java.io.*;
 
 import org.junit.jupiter.api.Test;
 public class PebbleGame {
     public static void main (String[] args){
-        System.out.println(getNumberOfPlayers());
+        int a = getNumberOfPlayers();
+        System.out.println(a);
+
+        createCSV(a);
+
     }
 
     public static int getNumberOfPlayers(){
@@ -25,6 +32,33 @@ public class PebbleGame {
         }
     }
 
+    public static void createCSV(int a){
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("C:\\Users\\georg\\OneDrive\\Documents\\GitHub\\Software-Development-CA\\src\\example.csv"));
+            StringBuilder sb = new StringBuilder();
+            
+            for (int x=0;x<3;x++){              
+                for (int i=0;i<a*11;i++){ 
+                    int randomInteger = (int)(Math.random() * 20+1);   
+                    sb.append(Integer.toString(randomInteger));
+                    sb.append(",");
+                }
+                sb.append("\n");
+            }
+
+            pw.write(sb.toString());
+            pw.close();
+
+            System.out.println("done");
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+
+
+
+
     public int higher(int x, int y) {
         if(x > y) 
             return x;
@@ -36,4 +70,13 @@ public class PebbleGame {
     public void higherTest() {
         assertEquals(5, higher(5, 3));
     }
+
+    @Test
+    public void getNumberOfPlayersInvalidInputTest(){
+        int a = getNumberOfPlayers();
+        if (a == -1){
+            fail("input must be an integer");
+        }
+    }
 }
+
